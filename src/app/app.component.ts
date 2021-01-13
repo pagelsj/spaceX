a613906ed54adcaccaa3c198dd08f6394c2aa24f
Jacques Pagels <jacques.pagels@hotmail.com>
2021-01-13 14:01:29 +0000

    Adding styling and service to recover data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpaceXApiService } from './services/index';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'spaceX';
+export class AppComponent implements OnInit {
+  launchData: any;
+
+  constructor (
+    private spaceXApiService: SpaceXApiService
+  ) { }
+
+  ngOnInit() {
+    this.spaceXApiService.load().subscribe(data => {
+      console.log("data: ", data);
+      this.launchData = data;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {
+  HeaderComponent,
+  LaunchDetailsComponent
+} from './components/index';
+import {
+  SpaceXApiService
+} from './services/index';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LaunchDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    SpaceXApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
