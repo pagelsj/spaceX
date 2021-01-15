cc77a30de2cc755ed84ff98335579b960276ba7c
Jacques Pagels <jacques.pagels@hotmail.com>
2021-01-15 16:16:13 +0000

    Fixing tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,28 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
+import {
+  HeaderComponent,
+  LaunchDetailsComponent
+} from './components/index';
+import {
+  SpaceXApiService,
+  ManipulateDataService
+} from './services/index';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        LaunchDetailsComponent
       ],
+      imports: [
+        HttpClientTestingModule
+      ]
     }).compileComponents();
   }));
 
@@ -16,16 +32,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'spaceX'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('spaceX');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('spaceX app is running!');
-  });
 });
--- src/app/services/spaceX-api/space-x-api.service.spec.ts
+++ src/app/services/spaceX-api/space-x-api.service.spec.ts
@@ -1,12 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
+import { environment } from 'src/environments/environment';
 import { SpaceXApiService } from './space-x-api.service';
 
 describe('SpaceXApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: SpaceXApiService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [SpaceXApiService]
+    });
+    service = TestBed.get(SpaceXApiService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: SpaceXApiService = TestBed.get(SpaceXApiService);
     expect(service).toBeTruthy();
+  });
+
+  it('should GET data to the SpaceX API', () => {
+    service.load()
+      .subscribe(() => { });
+
+    const req = httpMock.expectOne(
+      `${environment.BASE_API}/v3/launches?limit=10&order=desc`
+    );
+    expect(req.request.method).toEqual("GET");
   });
 });
